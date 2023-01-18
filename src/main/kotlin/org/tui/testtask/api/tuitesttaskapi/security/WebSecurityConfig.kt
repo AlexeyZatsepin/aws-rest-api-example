12cd99222ad202d3c1c8c85fc09ab4fc550004e7
Oleksii Zatsepin <oleksii.zatsepin@externals.adidas.com>
2023-01-18 16:03:45 +0800

    web security enabled property

--- src/main/kotlin/org/tui/testtask/api/tuitesttaskapi/security/WebSecurityConfig.kt
+++ src/main/kotlin/org/tui/testtask/api/tuitesttaskapi/security/WebSecurityConfig.kt
@@ -1,8 +1,8 @@
 package org.tui.testtask.api.tuitesttaskapi.security
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
@@ -11,10 +11,10 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 
 @EnableWebFluxSecurity
 @Configuration
-@Profile("!test")
 class WebSecurityConfig {
 
     @Bean
+    @ConditionalOnProperty(name = ["security.use-cognito-token"], havingValue = "true")
     fun springSecurityFilterChain(
         http: ServerHttpSecurity
     ): SecurityWebFilterChain {
@@ -29,11 +29,22 @@ class WebSecurityConfig {
                 "/webjars/**"
             ).permitAll()
             .anyExchange()
-            .permitAll()
-//            .authenticated()
+            .authenticated()
             .and()
             .oauth2ResourceServer()
             .jwt()
+        return http.build()
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = ["security.use-cognito-token"], havingValue = "false")
+    fun springDisabledSecurityFilterChain(
+        http: ServerHttpSecurity
+    ): SecurityWebFilterChain {
+        http.cors().and().csrf().disable()
+            .authorizeExchange()
+            .anyExchange()
+            .permitAll()
         return http.build()
     }
 }
