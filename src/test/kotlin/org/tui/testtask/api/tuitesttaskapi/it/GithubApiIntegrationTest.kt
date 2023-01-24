71317d6912750be918618dc9bef24f3cc579bc38
Alexey Zatsepin <alexzatsepin7@gmail.com>
2023-01-24 15:25:51 +0800

    update test scenarios

--- src/test/kotlin/org/tui/testtask/api/tuitesttaskapi/it/GithubApiIntegrationTest.kt
+++ src/test/kotlin/org/tui/testtask/api/tuitesttaskapi/it/GithubApiIntegrationTest.kt
@@ -2,6 +2,7 @@ package org.tui.testtask.api.tuitesttaskapi.it
 
 import com.github.tomakehurst.wiremock.client.WireMock.*
 import com.github.tomakehurst.wiremock.junit5.WireMockTest
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,6 +10,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.tui.testtask.api.tuitesttaskapi.model.Branch
 import org.tui.testtask.api.tuitesttaskapi.model.Repository
 
 private const val TEST_PATH = "/v1/user/octocat/repos"
@@ -60,6 +62,52 @@ class GithubApiIntegrationTest {
             .expectStatus().isOk
             .expectHeader().contentType(MediaType.APPLICATION_JSON)
             .expectBodyList(Repository::class.java)
+            .consumeWith<WebTestClient.ListBodySpec<Repository>> { result->
+                assertThat(result.responseBody!!).hasSize(1)
+                    .first()
+                    .extracting("name", "owner", "branches")
+                    .containsExactly("Hello-World", "octocat",
+                        listOf(Branch("master", "c5b97d5ae6c19d5c5df71a34c7fbeeda2479ccbc")))
+            }
+    }
+
+    @Test
+    fun whenRequestRepositoryShouldFilterForks_thenStatusShouldBeOk() {
+        stubFor(get(urlPathEqualTo(GITHUB_REPO_TEST_PATH))
+            .withHeader("Authorization", containing("sometoken"))
+            .withQueryParam("page", equalTo("1"))
+            .withQueryParam("per_page", equalTo("30"))
+            .withQueryParam("direction", equalTo("asc"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json; charset=utf-8")
+                .withBodyFile("github/repository-response-with-fork.json")))
+
+        stubFor(get(urlPathEqualTo(GITHUB_BRANCH_TEST_PATH))
+            .withHeader("Authorization", containing("sometoken"))
+            .withQueryParam("page", equalTo("1"))
+            .withQueryParam("per_page", equalTo("30"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json; charset=utf-8")
+                .withBodyFile("github/branches-response-page1.json")))
+
+        stubFor(get(urlPathEqualTo(GITHUB_BRANCH_TEST_PATH))
+            .withHeader("Authorization", containing("sometoken"))
+            .withQueryParam("page", equalTo("2"))
+            .withQueryParam("per_page", equalTo("30"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json; charset=utf-8")
+                .withBodyFile("github/empty-response.json")))
+
+        client.get()
+            .uri(TEST_PATH)
+            .header(HttpHeaders.AUTHORIZATION, "Bearer test")
+            .exchange()
+            .expectStatus().isOk
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBodyList(Repository::class.java).hasSize(1)
     }
 
 
@@ -80,7 +128,7 @@ class GithubApiIntegrationTest {
             .expectHeader().contentType(MediaType.APPLICATION_JSON)
             .expectBody()
             .jsonPath("$.status").isEqualTo("404")
-            .jsonPath("$.message").isEqualTo("Repository for octocat not found")
+            .jsonPath("$.message").isEqualTo("Repository for octocat not found.")
     }
 
 }
